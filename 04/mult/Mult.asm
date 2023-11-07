ce5d3dad622e6ab522a966bcc891ecfd1707514e
Kevin Hernandez <KFHEDU@gmail.com>
2023-11-06 19:51:36 -0800

    Adding completed Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,39 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@R2
+M=0
+
+@R1
+D=M
+
+@END
+D;JEQ
+
+@R0
+D=M
+
+@END
+D;JEQ
+
+(LOOP)
+
+@R0
+D=M
+
+@R2
+M=D+M		//Storing the result of i'th iteration in R2
+
+@R1
+D=M
+D=D-1
+M=D
+
+@LOOP		//Jump back to start of loop
+D;JNE	//Check to see if done
+
+(END)
+
+@END
+0;JMP		//Infinite Loop - End of program

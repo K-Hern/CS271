438c0fea5af7f2b50765c88f9adf227bbf263238
Kevin Hernandez <KFHEDU@gmail.com>
2023-11-08 21:31:56 -0800

    Adding completed Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+@24575
+D=A
+@screen_block 		//Initializing screen_block to contain 24,575 (Last Screen Pixel)
+M=D
+
+@8192
+D=A
+@i_remain		//Initializing i_remain to contain 8192 (number of pixels (-1's) within screen)
+M=D
+
+
+
+@KBD
+D=M
+
+@SET_D_BLACK
+D;JNE		//Checking to see if KBD Input is still 0; and checking again
+
+
+@input
+M=0
+
+@FILL_LOOP
+D;JEQ
+
+
+(SET_D_BLACK)
+
+@input
+M=-1		//Setting D to -1 if key pressed, or 0 if not
+
+
+
+
+(FILL_LOOP)	//Start of Screen Fill Loop
+
+@input
+D=M
+
+@screen_block
+A=M
+
+//@A - Setting A to Contents of screen_block
+M=D
+
+
+@screen_block
+D=M
+@1
+D=D-A
+@screen_block		//Decrementing screen_block by 1
+M=D
+
+
+@i_remain
+D=M
+@1
+D=D-A
+@i_remain		//Decrementing i_remain by 1
+M=D
+
+
+@FILL_LOOP		//Checking to see if screen is filled & exiting if true
+D;JNE
+
+
+@LOOP
+0;JMP
